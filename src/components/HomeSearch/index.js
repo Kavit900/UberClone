83a0549c2fe49c8a6c6973f678c3447661c94458
Kavit <kavitmht@gmail.com>
2021-04-10 18:15:52 -0700

    styled search component

--- src/components/HomeSearch/index.js
+++ src/components/HomeSearch/index.js
@@ -4,6 +4,8 @@ import {View, Text} from "react-native";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
+import styles from "./styles.js";
+
 const HomeSearch = (props) => {
   return (
     <View>
@@ -12,7 +14,7 @@ const HomeSearch = (props) => {
         <Text style={styles.inputText}>Where To?</Text>
 
         <View style={styles.timeContainer}>
-          <AntDesign name={'clockcircle'} size={16} />
+          <AntDesign name={'clockcircle'} size={16} color={'#535353'} />
           <Text>Now</Text>
           <MaterialIcons name={'keyboard-arrow-down'} size={16} />
         </View>
---
+++ src/components/HomeSearch/styles.js
@@ -0,0 +1,27 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  inputBox: {
+    backgroundColor: '#d9d9d9',
+    margin: 10,
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  inputText: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#6e6e6e',
+  },
+  timeContainer: {
+    flexDirection: 'row',
+    width: 100,
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 50
+  }
+});
+
+export default styles;
